36b724becb751a1ee5109312bd09b8209bc2e6f1
lukmanxpert <lukmanmiah2004@gmail.com>
2025-05-13 14:14:58 +0600

    updated banner images and optimized

--- src/pages/home/banner/Banner.jsx
+++ src/pages/home/banner/Banner.jsx
@@ -3,6 +3,9 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import image1 from "../../../assets/banner1.jpg"
+import image2 from "../../../assets/banner2.jpg"
+import image3 from "../../../assets/banner3.jpg"
 
 const Banner = () => {
     return (
@@ -26,7 +29,7 @@ const Banner = () => {
                     <div className="relative w-full h-full">
                         <img
                             className="w-full h-full object-cover"
-                            src="https://i.ibb.co.com/Ng0dPpC/romantic-couple-city-celebrating-engagement-together-min.jpg"
+                            src={image1}
                             alt="Romantic Couple"
                         />
                         <div className="absolute inset-0 bg-black bg-opacity-30 flex items-center justify-center">
@@ -43,7 +46,7 @@ const Banner = () => {
                     <div className="relative w-full h-full">
                         <img
                             className="w-full h-full object-cover"
-                            src="https://i.ibb.co.com/yhrxkKQ/groom-putting-ring-bride-s-finger-min.jpg"
+                            src={image2}
                             alt="Groom Putting Ring"
                         />
                         <div className="absolute inset-0 bg-black bg-opacity-30 flex items-center justify-center">
@@ -60,7 +63,7 @@ const Banner = () => {
                     <div className="relative w-full h-full">
                         <img
                             className="w-full h-full object-cover"
-                            src="https://i.ibb.co/Hdtc4xx/wedding-1836315-1920.jpg"
+                            src={image3}
                             alt="Wedding Ceremony"
                         />
                         <div className="absolute inset-0 bg-black bg-opacity-30 flex items-center justify-center">
